f94adbe99c1e669da66aad0cc9bbf4f34832fe30
irenemartiniglesias <i.martini@alumnos.urjc.es>
2016-11-15 19:45:37 +0100

    primer commit

--- client.py
+++ client.py
@@ -5,16 +5,27 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+# Dirección IP del servidor y puerto.
+METODO = sys.argv[1]
+DIRECCION = sys.argv[2]
+Linea0 = DIRECCION.split('@')
+Linea1 = Linea0[1]
+Linea2 = Linea1.split(':')
+SERVER = Linea2[0]
+PORT = int(Linea2[1])
 
 # Contenido que vamos a enviar
 LINE = '¡Hola mundo!'
 
+
+if len(sys.argv) != 3:
+    sys.exit('Usage: python client.py method receiver@IP:SIPport')
+
+        
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
