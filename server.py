35053aa96586fdeb0c27b034cc5bff9aadb10a48
irenemartiniglesias <i.martini@alumnos.urjc.es>
2016-11-21 20:38:21 +0100

    PEP8 y PEP257

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""Clase (y programa principal) para un servidor de eco en UDP simple"""
+"""Clase (y programa principal) para un servidor de eco en UDP simple."""
 
 import socketserver
 import sys
@@ -17,12 +17,10 @@ except:
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+"""Echo server class."""
 
     def handle(self):
-        """Escribe dirección y puerto del cliente (de tupla client_address)"""
+        """Escribe dirección y puerto del cliente (de tupla client_address)."""
         while 1:
             """Leyendo línea a línea lo que nos envía el cliente"""
             line = self.rfile.read()
