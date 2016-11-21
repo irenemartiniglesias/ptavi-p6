e4d04d9a73a7b3558a29c84ade054e3525904064
irenemartiniglesias <i.martini@alumnos.urjc.es>
2016-11-21 20:03:31 +0100

    con pep8

--- client.py
+++ client.py
@@ -18,18 +18,18 @@ try:
     PORT = int(Linea_partes.split(':')[1])
 except:
     sys.exit('Usage: python client.py method receiver@IP:SIPport')
-    
+ 
 if len(sys.argv) != 3:
     sys.exit('Usage: python client.py method receiver@IP:SIPport')
-    
+
 if PORT < 1024:
-     sys.exit('PORT INCORRET')
+    sys.exit('PORT INCORRET')
      
 """Contenido que vamos a enviar"""
 Linea_sip = ' sip:' + USUARIO + SERVER + ' SIP/2.0\r\n'
 LINEA = METODO + Linea_sip
 
-        
+
 """Creamos el socket, lo configuramos y lo atamos a un servidor/puerto"""
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -48,7 +48,7 @@ if lista == ['SIP/2.0 100 Trying', 'SIP/2.0 180 Ring', 'SIP/2.0 200 OK']:
     print ('Enviando: ' + LINEACK)
     my_socket.send(bytes(LINEACK, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-    
+
    
 """Cerramos todo"""
 my_socket.close()
